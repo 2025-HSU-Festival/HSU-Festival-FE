0c320b309a50408535f62022f8daba572c46d2e3
binniejung <ybe1027@naver.com>
2025-05-02 15:24:46 +0900

    🎨 Design: 메인 홈페이지 배너이미지, 네비게이션 바 UI 수정

--- src/styles/common/colors.js
+++ src/styles/common/colors.js
@@ -11,16 +11,17 @@ const colors = {
   // Text Colors
   text_primary: "#333333", // 기본 텍스트
   text_secondary: "#5c5c5c", // 보조 텍스트
-  text_gray: "#cccccc",
 
   // Background Colors
   bg_white: "#FFFFFF", // 밝은 배경
   bg_gray_light: "#f5f5f5", // 연회색 배경
-  bg_gray: "#cccccc", // 중간 회색 배경
+  bg_gray: "#cccccc",
+  nav_bg_gray: "#F9F9F9",
 
   // Common
   black: "#000000",
   white: "#FFFFFF",
+  gray: "#cccccc", // 중간 회색 배경
 };
 
 export default colors;
