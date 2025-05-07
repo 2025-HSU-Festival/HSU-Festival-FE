f0679fa9f2bd177505bc9833656179b74ec95bb0
binniejung <ybe1027@naver.com>
2025-05-07 17:54:45 +0900

    ♻️ Refactor: vite.config.js 프록시 설정 코드 삭제

--- vite.config.js
+++ vite.config.js
@@ -4,19 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://3.34.22.86:8080",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-        secure: false,
-      },
-      "^/notice.*": {
-        target: "http://3.34.22.86:8080",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
 });
