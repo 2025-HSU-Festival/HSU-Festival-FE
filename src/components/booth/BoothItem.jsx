3664758be8a1a153f400a240feab7187d875529e
khyaejin <olivia019182@naver.com>
2025-05-13 13:57:52 +0900

    ✨ Feat: 부스정보 - 부스 사진 클릭 시 자세히 볼 수 있도록 모달 추가

--- src/components/booth/BoothItem.jsx
+++ src/components/booth/BoothItem.jsx
@@ -2,16 +2,28 @@ import React from "react";
 
 // styles
 import * as S from "../../styles/booth/booth.styles";
+import SearchIcon from "../../assets/home/search.png";
+
 
 export default function BoothItem({
   booth,
   selectedMarker,
   setSelectedMarker,
+  onOpenModal, // 모달 오픈 함수 props로 받기
 }) {
+  const handleClick = () => {
+    setSelectedMarker(booth.boothNum);
+  };
+
+  const handleImageClick = (e) => {
+    e.stopPropagation(); // 부모 클릭 막기
+    onOpenModal?.(booth);
+  };
+
   return (
     <S.BoothItemRow
       selected={selectedMarker === booth.boothNum}
-      onClick={() => setSelectedMarker(booth.boothNum)}
+      onClick={handleClick} // ✅ marker만 설정
     >
       <S.BoothTextCol>
         <S.BoothName>
@@ -24,12 +36,14 @@ export default function BoothItem({
         <S.BoothLabel>활동</S.BoothLabel>
         <S.BoothDescription>{booth.description}</S.BoothDescription>
       </S.BoothTextCol>
-      <S.BoothImageBox>
+      <S.BoothImageBox onClick={handleImageClick}>
         <S.BoothImage
-          src={booth.imageUrl || "https://via.placeholder.com/80"}
+          src={booth.imageUrl || "https://hsu-festival-bucket.s3.ap-northeast-2.amazonaws.com/form/2025-05-13/a2f5a035-1b85-487d-a3eb-b4c5f0750388.png"}
           alt={booth.name}
         />
-      </S.BoothImageBox>
+        <S.IconHint>
+          <img src={SearchIcon} alt="상세 보기" />
+        </S.IconHint>      </S.BoothImageBox>
     </S.BoothItemRow>
   );
 }
